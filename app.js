2d4ef6f40a5b5fcbbbd866bcaf628b0610ea420a
tpires1041 <131817259+tpires1041@users.noreply.github.com>
2023-09-14 14:15:54 -0400

    commit 14 09

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index', { message: 'Olá, Mundo!' });
+  res.render('index');
 });
 
 app.listen(port, () => {
