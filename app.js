c900f2b45f72820bb9731dafae680436ea374e4f
tpires1041 <tiagopires2501@outlook.com>
2023-11-30 14:27:03 -0400

    commit 30 11 23
    muitos erros graves
    
    erro Error: Failed to lookup view "produtos"

--- app.js
+++ app.js
@@ -2,17 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const produtos = [
-  {id: 1, nome: "HONDA CIVIC", preco: "R$ 105.900", descricao: "2017/2017 2.0 16V FLEXONE EXL 4P CVT", imagem: "civic1.jpg"},
-  {id: 2, nome: "MITSUBISHI PAJERO SPORT", preco: "R$ 291.060", descricao: "2021/2022 2.4 16V MIVEC TURBO DIESEL HPE AWD AUTOMÁTICO", imagem: "pajero1.jpg"},
-  {id: 3, nome: "PORSCHE 911", preco: "R$ 1.860.000", descricao: "2022/2022 3.8 24V H6 GASOLINA TURBO S PDK", imagem: "porsche1.jpg"},
-]
-
-function buscarProdutoPorID(id){
-  const produto = produtos.find(produto => produto.id == id);
-  return produto || null
-}
-
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
@@ -20,21 +9,28 @@ app.get('/', (req, res) => {
   res.render("produtos", {produtos});
 });
 
-app.get('/civic.ejs', (req, res) => {
+const produtos = [
+  {id: 1, nome: "HONDA CIVIC", preco: "R$ 105.900", descricao: "2017/2017 2.0 16V FLEXONE EXL 4P CVT", imagem: "civic1.jpg"},
+  {id: 2, nome: "MITSUBISHI PAJERO SPORT", preco: "R$ 291.060", descricao: "2021/2022 2.4 16V MIVEC TURBO DIESEL HPE AWD AUTOMÁTICO", imagem: "pajero1.jpg"},
+  {id: 3, nome: "PORSCHE 911", preco: "R$ 1.860.000", descricao: "2022/2022 3.8 24V H6 GASOLINA TURBO S PDK", imagem: "porsche1.jpg"},
+  {id: 4, nome: "PORSCHE 911", preco: "R$ 1.860.000", descricao: "2022/2022 3.8 24V H6 GASOLINA TURBO S PDK", imagem: "porsche1.jpg"},
+  {id: 5, nome: "PORSCHE 911", preco: "R$ 1.860.000", descricao: "2022/2022 3.8 24V H6 GASOLINA TURBO S PDK", imagem: "porsche1.jpg"},
+  {id: 6, nome: "PORSCHE 911", preco: "R$ 1.860.000", descricao: "2022/2022 3.8 24V H6 GASOLINA TURBO S PDK", imagem: "porsche1.jpg"},
+  {id: 7, nome: "PORSCHE 911", preco: "R$ 1.860.000", descricao: "2022/2022 3.8 24V H6 GASOLINA TURBO S PDK", imagem: "porsche1.jpg"},
+  {id: 8, nome: "PORSCHE 911", preco: "R$ 1.860.000", descricao: "2022/2022 3.8 24V H6 GASOLINA TURBO S PDK", imagem: "porsche1.jpg"},
+]
+
+function buscarProdutoPorID(id){
+  const produto = produtos.find(produto => produto.id == id);
+  return produto || null
+}
+
+app.get('/produtos/:id', (req, res) => {
+  const produto = buscarProdutoPorID(req.params.id)
+  res.render('produtos', {produto});
+});
+
   res.render('civic');
-});
-
-app.get('/index.ejs', (req, res) => {
-  res.render('index');
-});
-
-app.get('/pajero.ejs', (req, res) => {
-  res.render('pajero');
-});
-
-app.get('/porsche.ejs', (req, res) => {
-  res.render('porsche');
-});
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
