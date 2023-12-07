66f6ef7912ba064b88b64368c75bd6115b789f0f
tpires1041 <tiagopires2501@outlook.com>
2023-12-07 01:51:10 -0400

    commit 05 12 23
    falta carregar as imagens

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.render("produtos", {produtos});
-});
+
 
 const produtos = [
   {id: 1, nome: "HONDA CIVIC", preco: "R$ 105.900", descricao: "2017/2017 2.0 16V FLEXONE EXL 4P CVT", imagem: "civic1.jpg"},
@@ -18,19 +16,26 @@ const produtos = [
   {id: 6, nome: "PORSCHE 911", preco: "R$ 1.860.000", descricao: "2022/2022 3.8 24V H6 GASOLINA TURBO S PDK", imagem: "porsche1.jpg"},
   {id: 7, nome: "PORSCHE 911", preco: "R$ 1.860.000", descricao: "2022/2022 3.8 24V H6 GASOLINA TURBO S PDK", imagem: "porsche1.jpg"},
   {id: 8, nome: "PORSCHE 911", preco: "R$ 1.860.000", descricao: "2022/2022 3.8 24V H6 GASOLINA TURBO S PDK", imagem: "porsche1.jpg"},
+  {id: 9, nome: "PORSCHE 911", preco: "R$ 1.860.000", descricao: "2022/2022 3.8 24V H6 GASOLINA TURBO S PDK", imagem: "porsche1.jpg"},
+  {id: 10, nome: "PORSCHE 911", preco: "R$ 1.860.000", descricao: "2022/2022 3.8 24V H6 GASOLINA TURBO S PDK", imagem: "porsche1.jpg"},
 ]
+
 
 function buscarProdutoPorID(id){
   const produto = produtos.find(produto => produto.id == id);
   return produto || null
 }
 
+app.get('/', (req, res) => {
+  res.render('index', {produtos});
+});
+
+
 app.get('/produtos/:id', (req, res) => {
   const produto = buscarProdutoPorID(req.params.id)
   res.render('produtos', {produto});
 });
 
-  res.render('civic');
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
